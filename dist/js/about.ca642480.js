c68f16ace7849c018c6e17854c24f7cf3b274273
Raj Jagirdar <133992843+rajjagirdar007@users.noreply.github.com>
2025-02-19 16:20:53 -0500

    fs

---
+++ dist/js/about.ca642480.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkkhr"]=self["webpackChunkkhr"]||[]).push([[594],{603:function(n,u,r){r.r(u),r.d(u,{default:function(){return i}});var t=r(6768);const a={class:"about"};function e(n,u){return(0,t.uX)(),(0,t.CE)("div",a,u[0]||(u[0]=[(0,t.Lk)("h1",null,"This is an about page",-1)]))}var s=r(1241);const c={},h=(0,s.A)(c,[["render",e]]);var i=h}}]);
+//# sourceMappingURL=about.ca642480.js.map
